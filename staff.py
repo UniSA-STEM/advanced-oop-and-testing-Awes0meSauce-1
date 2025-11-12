b73b3ed49ebdc8ae2368976869491112cc98e136
Awes0meSauce <78591427+Awes0meSauce@users.noreply.github.com>
2025-11-13 05:25:01 +1030

    Added the basic functions for the staff.py class

--- staff.py
+++ staff.py
@@ -1,8 +1,28 @@
-'''
+"""
 File: filename.py
 Description: A brief description of this Python module.
 Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+"""
+
+class Staff:
+    def __init__(self, role, responsibilities):
+        self.__role = role
+        self.__responsibilities = responsibilities
+
+    def get_role(self):
+        return self.__role
+
+    def get_responsibilities(self):
+        return self.__responsibilities
+
+    def feed_animal(self, animal):
+        pass
+
+    def clean_enclosure(self):
+        pass
+
+    def health_check(self):
+        pass
